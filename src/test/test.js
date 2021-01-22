5bfce73b88791461975f321dc024118f21b471dc
Lee Campbell <lee.campbell@evergreen-life.co.uk>
2021-01-22 14:43:36 +0000

    Removed generated JavaScript.

--- src/test/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../index");
-console.log(index_1.listPermutations("bone", {
-    language: "en",
-    sortOrder: "alphabetical",
-    keepOriginal: false
-}));
-//# sourceMappingURL=test.js.map
